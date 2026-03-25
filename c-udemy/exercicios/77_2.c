57ef215f3d3475456abdff2a157d25d294fe2dbb
liatsuki <lidia.duarte.goncalves@gmail.com>
2026-03-25 10:24:20 +0000

    250326_1024

--- c-udemy/exercicios/77_2.c
+++ c-udemy/exercicios/77_2.c
@@ -1,23 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    char texto[3];
-
-    printf("Idade: ");
-    scanf("%s", texto);
-
-    int idade = atoi(texto);
-
-    printf("\n");
-
-    printf("Idade = %d\n", idade);
-
-    if (idade >= 18) {
-        printf("Maior de idade\n");
-    } else {
-        printf("Menor de idade\n");
-    }
-
-    return 0;
+    
 }
