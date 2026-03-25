24f5e56e7de4b6e1bae037f6630f62e4f12f7160
liatsuki <lidia.duarte.goncalves@gmail.com>
2026-03-25 12:25:02 +0000

    250326_1224

---
+++ treinoArray/soma_media.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num[4], soma = 0, media; // num[0] num[1] num[2] num[3] num[4]
+
+    for (int i = 0; i < 5; i++) {
+        printf("Numero %d: ", i+1);
+        scanf("%d", &num[i]);
+
+        soma += num[i];
+        media = soma / 5;
+    }
+
+    printf("\nSoma: %d\n", soma);
+    printf("Media: %d\n", media);
+}
