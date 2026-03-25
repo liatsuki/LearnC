aa53c44d6d0dc780dc18730c0117091245e81fcf
liatsuki <lidia.duarte.goncalves@gmail.com>
2026-03-25 12:33:17 +0000

    250326_1233

--- treinoArray/soma_media.c
+++ treinoArray/soma_media.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int num[4], soma = 0, media; // num[0] num[1] num[2] num[3] num[4]
+    int num[4], soma = 0, media; // num[0] num[1] num[2] num[3]
 
     for (int i = 0; i < 5; i++) {
         printf("Numero %d: ", i+1);
