064bd3cb0214c27e855968a453146f646e116f23
Lídia Gonçalves <lidia.duarte.goncalves@gmail.com>
2026-04-29 18:21:21 +0100

    290426_1821

---
+++ c-udemy/exercicios/124_6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int fila[10];
+int inicio = 0, fim = 0;
+int tamanho_maximo = 10;
+
+void enqueue(int posicao) {
+    if (fim == tamanho_maximo) {
+        printf("Fila cheia!\n");
+        return;
+    }
+}
+
+int main() {
+
+}
