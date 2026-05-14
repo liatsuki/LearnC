8a524160b6d626b3e07322a30f4e358ccad98e60
liatsuki <lidia.duarte.goncalves@gmail.com>
2026-05-14 17:10:28 +0100

    140526_1710

---
+++ teste-headers/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "calculos.h"
+
+int main () {
+    printf("Usando headers\n");
+
+    int y = 5;
+
+    int quad = quadrado(y);
+    int cub = cubo(y);
+
+    printf("Quadrado de %d: %d\n", y, quad);
+    printf("Cubo de %d: %d\n", y, cub);
+
+    printf("Valor da constante PI: %f\n", _PI);
+
+    return 0;
+}
