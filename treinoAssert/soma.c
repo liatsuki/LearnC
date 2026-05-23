b2e8980d37f3560a2543c9aa586f6d06ae49c443
Lídia Gonçalves <lidia.duarte.goncalves@gmail.com>
2026-05-23 14:59:22 +0100

    230526_1459

---
+++ treinoAssert/soma.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+#include <stdio.h>
+
+int soma(int a, int b) { // Soma
+    return a + b;
+}
+
+void testar_soma() { // testes
+// assert(soma(a, b) == a + b);
+    assert(soma(2, 3) == 5);
+    assert(soma(0, 0) == 0);
+    assert(soma(-1, 1) == 0);
+    assert(soma(-2, -3) == -5);
+    assert(soma(2, 1) == 10); // Assertion failed: soma(2, 1) == 10, file soma.c, line 14
+}
+
+int main() {
+    testar_soma(); // chamar a funcao dos testes
+    return 0;
+}
